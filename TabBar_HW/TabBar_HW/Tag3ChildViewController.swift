65a12ff4b629c71a0fc3bf4547c3c1fadd187115
Dapucla <55713843+Dapucla@users.noreply.github.com>
2020-05-24 15:42:15 +0300

    Add files via upload

---
+++ TabBar_HW/TabBar_HW/Tag3ChildViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Tag3ChildViewController.swift
+//  TabBar_HW
+//
+//  Created by Даниил Алексеев on 03.05.2020.
+//  Copyright © 2020 Даниил Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class Tag3ChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
---
+++ TabBar_HW/TabBar_HW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBar_HW
+//
+//  Created by Даниил Алексеев on 02.05.2020.
+//  Copyright © 2020 Даниил Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
