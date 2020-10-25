6a8a4981f4207a170d53bd1679f8d27aa2eb853b
Dapucla <55713843+Dapucla@users.noreply.github.com>
2020-10-26 00:20:40 +0300

    Add files via upload

---
+++ Таблицы и коллекции/collections&tablesHW/collections&tablesHW/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  collections&tablesHW
+//
+//  Created by Даниил Алексеев on 11.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    class Clothes {
+        var name: String = ""
+        var image = UIImage()
+        var previousPrice = ""
+        var actualPrice = ""
+        var decount = ""
+        
+        init(name: String, image: UIImage, previousPrice: String, actualPrice: String, decount: String  ) {
+            self.name = name
+            self.image = image
+            self.previousPrice = previousPrice
+            self.actualPrice = actualPrice
+            self.decount = decount
+        }
+    }
+    
+    struct clothes {
+        let names = ["Куртка","Пальто","Пиджак","Джинсы", "Свитер","Жилет"]
+        let images = [UIImage(named: "jacket"),UIImage(named: "coat"),UIImage(named: "blazer"),UIImage(named: "jeans"),UIImage(named: "sweater"),UIImage(named: "vest")]
+        let previousPrice = ["11000","18500","8300","5650","4250","7440"]
+        let actualPrice = ["8950", "13100", "6750", "4250", "3300", "5170"]
+        let decount = ["-20%", "-30%", "-25%", "-27%", "-32%", "-44%"]
+    }
+    
+//    var jacket = Clothes(name: "Куртка", image: UIImage(named: "jacket")!, previousPrice: "11000", actualPrice: "8950", decount: "-20%")
+//    let names = ["Куртка","Пальто","Пиджак","Джинсы", "Свитер","Жилет"]
+//    let images = [UIImage(named: "jacket"),UIImage(named: "coat"),UIImage(named: "blazer"),UIImage(named: "jeans"),UIImage(named: "sweater"),UIImage(named: "vest")]
+//    let previousPrice = ["11000","18500","8300","5650","4250","7440"]
+//    let actualPrice = ["8950", "13100", "6750", "4250", "3300", "5170"]
+//    let decount = ["-20%", "-30%", "-25%", "-27%", "-32%", "-44%"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        let w = UIScreen.main.bounds.size.width / 2.5
+        let h = UIScreen.main.bounds.size.height / 2.7
+        return CGSize(width: w, height: h  )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.nameLabel.text = actualPrice[indexPath.row]
+        cell.imageView.image = images[indexPath.row]
+        cell.previousPriceLaabel.text = previousPrice[indexPath.row]
+        cell.productDescribtion.text = names[indexPath.row]
+        cell.discount.text = decount[indexPath.row]
+        
+        cell.layer.borderWidth = 0.3
+        cell.layer.borderColor = UIColor.lightGray.cgColor
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate{
+     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            if let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Header", for: indexPath) as? CollectionReusableView{
+                headerView.titleLaberl.text = "Скидки"
+               
+                return headerView
+            }
+        default:
+            return UICollectionReusableView()
+        }
+        return UICollectionReusableView()
+    }
+}
