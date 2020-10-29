40c3bd839587c17f39d634e44de23e437904b42e
Dapucla <55713843+Dapucla@users.noreply.github.com>
2020-10-30 00:54:13 +0300

    Fix

--- Создание собственных UI компонентов/personalUIcomponents/personalUIcomponents/ClockView.swift
+++ Создание собственных UI компонентов/personalUIcomponents/personalUIcomponents/ClockView.swift
@@ -7,12 +7,8 @@
 
 import UIKit
 
-
-
 @IBDesignable
  class ClockView: UIView {
-    
-    var isSetuped = false
     
     var markerSize: CGFloat = 4
     var markerLength: CGFloat = 12
@@ -21,21 +17,21 @@ import UIKit
     @IBInspectable var hourLineOffset: CGFloat = 50
     @IBInspectable var hourLineSize: CGFloat = 7
     @IBInspectable var hourLineColor: UIColor = UIColor.green
-    var hours: CGFloat = 9 {
+    var hours: CGFloat = 6 {
         didSet { updateHours() }
     }
     
     @IBInspectable var secondsLineOffset: CGFloat = 15
     @IBInspectable var secondsLineSize: CGFloat = 2
     @IBInspectable var secondsLineColor: UIColor = UIColor.red
-    var seconds: CGFloat = 30 {
+    var seconds: CGFloat = 15 {
         didSet { updateSeconds() }
     }
     
     @IBInspectable var minutsLineOffset: CGFloat = 30
     @IBInspectable var minutsLineSize: CGFloat = 5
     @IBInspectable var minutsLineColor: UIColor = UIColor.purple
-    var minuts: CGFloat = 15 {
+    var minuts: CGFloat = 5 {
         didSet { updateMinuts() }
     }
     
@@ -51,48 +47,60 @@ import UIKit
     private let secondsLine = UIView()
     private let minutsLine = UIView()
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        let w = frame.size.width
-        let h = frame.size.height
-        
-        hourLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        hourLine.frame = CGRect(x: w / 2 - hourLineSize / 2, y: hourLineOffset, width: hourLineSize, height: h/2 - hourLineOffset)
-        hourLine.backgroundColor = hourLineColor
-        
-        secondsLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        secondsLine.frame = CGRect(x: w / 2 - secondsLineSize / 2, y: secondsLineOffset, width: secondsLineSize, height: h/2 - secondsLineOffset)
-        secondsLine.backgroundColor = secondsLineColor
-        
-        minutsLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        minutsLine.frame = CGRect(x: w / 2 - minutsLineSize / 2, y: minutsLineOffset, width: minutsLineSize, height: h/2 - minutsLineOffset)
-        minutsLine.backgroundColor = minutsLineColor
-        
-        updateHours()
-        updateSeconds()
-        updateMinuts()
-        
-        topMarker.frame = CGRect(x: w / 2 - markerSize / 2, y: 0, width: markerSize, height: markerLength)
-        leftMarker.frame = CGRect(x: 0, y: h / 2 - markerSize / 2, width: markerLength, height: markerSize)
-        rightMarker.frame = CGRect(x: w - markerLength, y: h / 2 - markerSize / 2, width: markerLength, height: markerSize)
-        bottomMarker.frame = CGRect(x: w / 2 - markerSize / 2, y: h - markerLength, width: markerSize, height: markerLength)
-       
+   //ИНИЦИАЛЕЗИРУЕМ VIEW
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+       setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder ) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    func setUp() {
         roundedView.backgroundColor = roundedViewColor
-        roundedView.frame = CGRect(x: w / 2 - 8, y: h / 2 - 8, width: 16, height: 16)
-        roundedView.layer.cornerRadius = 8
+        minutsLine.backgroundColor =  minutsLineColor
+        secondsLine.backgroundColor = secondsLineColor
+        hourLine.backgroundColor = hourLineColor
         
         for v in [topMarker, leftMarker, rightMarker, bottomMarker]{
             v.backgroundColor = markerColor
         }
         
-        layer.cornerRadius = frame.size.width / 2
-        
-        if isSetuped { return }
-        isSetuped = true
-        
         for v in [topMarker, leftMarker, rightMarker, bottomMarker, hourLine, secondsLine, minutsLine, roundedView]{
             addSubview(v)
         }
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let w = frame.size.width
+        let h = frame.size.height
+    
+        hourLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        hourLine.frame = CGRect(x: w / 2 - hourLineSize / 2, y: hourLineOffset, width: hourLineSize, height: h/2 - hourLineOffset)
+       
+        secondsLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        secondsLine.frame = CGRect(x: w / 2 - secondsLineSize / 2, y: secondsLineOffset, width: secondsLineSize, height: h/2 - secondsLineOffset)
+        
+        minutsLine.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        minutsLine.frame = CGRect(x: w / 2 - minutsLineSize / 2, y: minutsLineOffset, width: minutsLineSize, height: h/2 - minutsLineOffset)
+    
+        updateHours()
+        updateSeconds()
+        updateMinuts()
+    
+        topMarker.frame = CGRect(x: w / 2 - markerSize / 2, y: 0, width: markerSize, height: markerLength)
+        leftMarker.frame = CGRect(x: 0, y: h / 2 - markerSize / 2, width: markerLength, height: markerSize)
+        rightMarker.frame = CGRect(x: w - markerLength, y: h / 2 - markerSize / 2, width: markerLength, height: markerSize)
+        bottomMarker.frame = CGRect(x: w / 2 - markerSize / 2, y: h - markerLength, width: markerSize, height: markerLength)
+        
+        roundedView.frame = CGRect(x: w / 2 - 8, y: h / 2 - 8, width: 16, height: 16)
+        roundedView.layer.cornerRadius = 8
+        
+        layer.cornerRadius = frame.size.width / 2
     }
     
     func updateHours(){
--- Создание собственных UI компонентов/personalUIcomponents/personalUIcomponents/SegmentedView.swift
+++ Создание собственных UI компонентов/personalUIcomponents/personalUIcomponents/SegmentedView.swift
@@ -7,62 +7,56 @@
 
 import UIKit
 
-
-
 protocol SegmentedViewDelegate: NSObjectProtocol {
     func segmentedViewIndex(_ segmentedView: SegmentedView, labelText: String)
 }
+
+@IBDesignable
 class SegmentedView: UIView {
     
-  
-    
-   
-
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
-    @IBInspectable var segmentedControlColor: UIColor = UIColor.darkGray
-    @IBInspectable var segmentTitle1: String = "Первый"
-    @IBInspectable var segmentTitle2: String = "Второй"
-    @IBInspectable var segmentTintColor: UIColor = UIColor.systemTeal
-    
-    
+    @IBOutlet weak var segmentBackground: UIView!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
     @IBOutlet weak var label: UILabel!
     
     weak var delegate: SegmentedViewDelegate?
     
-    @IBAction func indexChanged(_ sender: Any) {
-        switch segmentedControl.selectedSegmentIndex
-            {
-            case 0:
-                delegate?.segmentedViewIndex(self, labelText: "Первый")
-            case 1:
-                delegate?.segmentedViewIndex(self, labelText: "Второй")
-            default:
-                break
-            
+    @IBInspectable var firstButtonText: String = "Первый"
+    @IBInspectable var secondButtonText: String = "Второй"
+    @IBInspectable var segmentBackgroundColor: UIColor = .darkGray
+    @IBInspectable var ButtonBackgroundColor: UIColor = .systemYellow
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        segmentBackground.layer.cornerRadius = 10
+        firstButton.layer.cornerRadius = 5
+        secondButton.layer.cornerRadius = 5
+        secondButton.backgroundColor = .none
+        firstButton.setTitle(firstButtonText, for: .normal)
+        secondButton.setTitle(secondButtonText, for: .normal)
+        segmentBackground.backgroundColor = segmentBackgroundColor
+        firstButton.backgroundColor = ButtonBackgroundColor
+        secondButton.backgroundColor = .none
+    }
+
+    @IBAction func firstButton(_ sender: Any) {
+        delegate?.segmentedViewIndex(self, labelText: "Первый")
+        UIView.animate(withDuration: 0.3){
+            self.firstButton.backgroundColor = self.ButtonBackgroundColor
+            self.secondButton.backgroundColor = .none
         }
     }
     
-    
-    var isSetuped = false
-    
-    override func layoutSubviews() {
-   
-        segmentedControl.backgroundColor = segmentedControlColor
-        segmentedControl.setTitle(segmentTitle1, forSegmentAt: 0)
-        segmentedControl.setTitle(segmentTitle2, forSegmentAt: 1)
-        segmentedControl.selectedSegmentTintColor = segmentTintColor
-       
-        if isSetuped { return }
-        isSetuped = true
+    @IBAction func secondButton(_ sender: Any) {
+        delegate?.segmentedViewIndex(self, labelText: "Второй")
+        UIView.animate(withDuration: 0.3){
+            self.firstButton.backgroundColor = .none
+            self.secondButton.backgroundColor = self.ButtonBackgroundColor
+        }
     }
     
     static func loadFromNIB() -> SegmentedView{
         let nib = UINib(nibName: "SegmentedView", bundle: nil)
         return nib.instantiate(withOwner: self, options: nil).first as! SegmentedView
     }
-    
-    
-    
-    
 }
